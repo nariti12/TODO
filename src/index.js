828a69d7b4f18a717cf982c1574ecb9a13e684b8
g n <nariti12@gmail.com>
2022-11-19 14:31:03 +0000

    2331

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const onClickAdd = () => {
 
   //div作成
   const div = document.createElement("div");
+  div.className = "list-row";
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
   console.log(div);
 };
 
