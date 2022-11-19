a53b92bfd5138137503d7279785a7d5178125d56
g n <nariti12@gmail.com>
2022-11-19 14:20:28 +0000

    Update 15 files

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -176,6 +176,17 @@ module.hot.accept(reloadCSS);
 "use strict";
 
 require("./styles.css");
+var onClickAdd = function onClickAdd() {
+  var inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div作成
+  var div = document.createElement("div");
+  console.log(div);
+};
+document.getElementById("add-button").addEventListener("click", function () {
+  return onClickAdd();
+});
 },{"./styles.css":"src/styles.css"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
@@ -201,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46329" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "39545" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+
+  //div作成
+  const div = document.createElement("div");
+  console.log(div);
+};
+
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
